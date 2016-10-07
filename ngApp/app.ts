c055fdeecd9a25c5292a2a566ba67d5827f48dd7
smitha <smitha.gopalan26@gmail.com>
2016-10-07 13:20:56 -0700

    upload

---
+++ ngApp/app.ts
@@ -0,0 +1,36 @@
+namespace upload {
+
+    angular.module('upload', ['ui.router', 'ngResource', 'ui.bootstrap']).config((
+        $stateProvider: ng.ui.IStateProvider,
+        $urlRouterProvider: ng.ui.IUrlRouterProvider,
+        $locationProvider: ng.ILocationProvider
+    ) => {
+        // Define routes
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: '/ngApp/views/home.html',
+                controller: upload.Controllers.HomeController,
+                controllerAs: 'controller'
+            })
+            .state('about', {
+                url: '/about',
+                templateUrl: '/ngApp/views/about.html',
+                controller: upload.Controllers.AboutController,
+                controllerAs: 'controller'
+            })
+            .state('notFound', {
+                url: '/notFound',
+                templateUrl: '/ngApp/views/notFound.html'
+            });
+
+        // Handle request for non-existent route
+        $urlRouterProvider.otherwise('/notFound');
+
+        // Enable HTML5 navigation
+        $locationProvider.html5Mode(true);
+    });
+
+    
+
+}
